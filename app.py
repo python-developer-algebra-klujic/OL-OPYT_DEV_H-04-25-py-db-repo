6be33524c9a8b3c1ca1211d048377f621ed99c7a
Python Developer Algebra Krunoslav Lujic <krunoslav.lujic@predavaci.algebra.hr>
2025-11-10 18:22:40 +0100

    Renamed module name typo

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from models.authors import Author
 from models.books import Book
 from repositories.author_repos import add_author
-from repositories.bd_init import db_init
+from repositories.db_init import db_init
 from repositories.book_repos import add_book
 
 
